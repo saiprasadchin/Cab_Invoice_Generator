723edd46573feabd60165f134706a99cd8f54319
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-07-10 21:15:56 -0700

    Added: Test case for to get minimum fare

--- src/main/java/com/cabinvoice/InvoiceService.java
+++ src/main/java/com/cabinvoice/InvoiceService.java
@@ -1,11 +1,12 @@
 package com.cabinvoice;
 
 public class InvoiceService {
-    private static final double MINIMUM_COST_PER_KM = 10;
-    private static final double COST_PER_TIME = 1;
+    public static final double MINIMUM_COST_PER_KM = 10;
+    public static final double COST_PER_TIME = 1;
+    public static final int MINIMUM_FARE = 5;
 
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
-        return totalFare;
+        return Math.max(MINIMUM_FARE, totalFare);
     }
 }
--- src/test/java/com/cabinvoice/InvoiceServiceTest.java
+++ src/test/java/com/cabinvoice/InvoiceServiceTest.java
@@ -12,4 +12,13 @@ public class InvoiceServiceTest {
         double fare = invoiceService.calculateFare(distance, time);
         Assert.assertEquals(25, fare , 0.0);
     }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinumamFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(5, fare , 0.0);
+    }
 }

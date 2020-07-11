9044dc78847c159067ee9defdbd764381086bfc8
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-07-10 21:23:33 -0700

    Added: InvoiceSummary class and Ride class

---
+++ src/main/java/com/cabinvoice/Ride.java
@@ -0,0 +1,11 @@
+package com.cabinvoice;
+
+public class Ride {
+    private int time;
+    private double distance;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
--- src/test/java/com/cabinvoice/InvoiceServiceTest.java
+++ src/test/java/com/cabinvoice/InvoiceServiceTest.java
@@ -1,12 +1,18 @@
 package com.cabinvoice;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+    InvoiceService invoiceService = null;
+
+    @Before
+    public void setUp() throws Exception{
+        invoiceService = new InvoiceService();
+    }
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceService invoiceService = new InvoiceService();
         double distance = 2;
         int time = 5;
         double fare = invoiceService.calculateFare(distance, time);
@@ -15,10 +21,19 @@ public class InvoiceServiceTest {
 
     @Test
     public void givenDistanceAndTime_ShouldReturnMinumamFare() {
-        InvoiceService invoiceService = new InvoiceService();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceService.calculateFare(distance, time);
         Assert.assertEquals(5, fare , 0.0);
+    }
+
+    @Test
+    public void giveMultipleRides_shouldReturnInvoiceSummary() {
+        Ride[] rides = {new Ride(2.0,5),
+                new Ride(0.1,1),
+        };
+        /*InvoiceSummary summary = invoiceService.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,30.0);
+        Assert.assertEquals(expectedInvoiceSummary, summary);*/
     }
 }
